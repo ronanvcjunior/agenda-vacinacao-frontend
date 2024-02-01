eb521885dea0cd7eb2ba84670fbc93827fb35d87
Ronan Vieira do Carmo Juior <ronanvcjunior@gmail.com>
2024-01-31 22:15:48 -0300

    refac: corrigindo bug de info-tabela

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
-import {InfoTabelaComponent} from "./info-tabela/info-tabela.component";
 import {InfoAlergiaComponent} from "./info-alergia/info-alergia.component";
 import {InfoUsuarioComponent} from "./info-usuario/info-usuario.component";
 import {InfoVacinaComponent} from "./info-vacina/info-vacina.component";
 import {InfoAgendaComponent} from "./info-agenda/info-agenda.component";
 
 export const routes: Routes = [
-  { path: 'info/:path', component: InfoTabelaComponent },
   { path: 'alergia', component: InfoAlergiaComponent },
   { path: 'usuario', component: InfoUsuarioComponent },
   { path: 'vacina', component: InfoVacinaComponent },
